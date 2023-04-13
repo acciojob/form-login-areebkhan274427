c91589fe41ee6660e44261201bea33b8b70c898f
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-04-13 13:06:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	var first = document.querySelector('[name="fname"]').value;
+	var last = document.querySelector('[name="lname"]').value;
+	alert(first+" "+last);
 
 }
